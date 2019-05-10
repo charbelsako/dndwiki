2ab9fe62074a586f1e15f7815df6f94b9c753397
charbel sarkis <charbelsako@gmail.com>
2019-05-10 08:44:40 +0300

    preprocessed the data

---
+++ js/preprocess.js
@@ -0,0 +1,21 @@
+// node script
+const data = require('./../data/bestiary-mm.json').monster
+
+const fs = require('fs')
+// data has been loaded
+
+// preprocess the data
+function preprocess(data) {
+  // for each monster check if it has an image
+  let i = 0
+  for (let i = 0; i < data.length; i++) {
+    try {
+      fs.readFileSync(`../img/bestiary/MM/${data[i].name}.jpg`)
+      data[i].image = `../img/bestiary/MM/${data[i].name}.jpg`
+    } catch (err) {}
+  }
+  newData = JSON.stringify(data)
+  // fs.writeFileSync('../data/new-bestiary-mm.json', newData)
+}
+
+preprocess(data)
