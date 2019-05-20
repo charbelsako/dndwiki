aeac43c6e09b7bd92e773b262affb612726af55c
RottingToxin <sassinerichard@gmail.com>
2019-05-20 03:25:21 +0300

    Added base popup layout and functionality

--- js/clear-page.js
+++ js/clear-page.js
@@ -1,5 +1,5 @@
 function clear_page(element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild)
-  }
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
 }
--- js/spells.js
+++ js/spells.js
@@ -1,40 +1,40 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const data = await loadData('../data/full-spells.json')
-  console.log(data[0])
-  function generateSpellTable(num) {
-    let i = 0
-    for (const spell of data) {
-      i++
-      if (i == num) return
-      spellContainer.innerHTML += `
-        <td>
-          ${spell.name}
-        </td>
-        <td>
-          ${spell.level}
-        </td>
-        <td>
-          ${` ${spell.time[0].unit} ${spell.time[0].number}`}
-        </td>
-        <td>
-          ${spell.concentration}
-        </td>
-        <td>
-          ${spell.school}
-        </td>
-        <td class="onlargeonly">
-          ${`${spell.range.type} ${spell.range.amount ? spell.range.amount : ''} ${
-            spell.range.distance ? spell.range.distance.type : ''
-          }`}
-        </td>
-        <td class="onlargeonly">
-          ${spell.source}
-        </td>
-      `
+    const data = await loadData('../data/full-spells.json')
+    console.log(data[0])
+    function generateSpellTable(num) {
+        let i = 0
+        for (const spell of data) {
+            i++
+            if (i == num) return
+            spellContainer.innerHTML += `
+            <td>
+              ${spell.name}
+            </td>
+            <td>
+              ${spell.level}
+            </td>
+            <td>
+              ${` ${spell.time[0].unit} ${spell.time[0].number}`}
+            </td>
+            <td>
+              ${spell.concentration}
+            </td>
+            <td>
+              ${spell.school}
+            </td>
+            <td class="onlargeonly">
+              ${`${spell.range.type} ${spell.range.amount ? spell.range.amount : ''} ${
+                spell.range.distance ? spell.range.distance.type : ''
+              }`}
+            </td>
+            <td class="onlargeonly">
+              ${spell.source}
+            </td>
+              `
+        }
     }
-  }
-  generateSpellTable(10)
-  // still needs pagination
-  // sorting by name type blah blah blah.
-  // search function
+    generateSpellTable(10)
+    // still needs pagination
+    // sorting by name type blah blah blah.
+    // search function
 })
