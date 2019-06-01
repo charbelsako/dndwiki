90abddfee0fb385cfca08fd7ce3941163b3e522d
charbel sarkis <charbelsako@gmail.com>
2019-06-01 22:16:49 +0300

    added pagination for the spells table

--- js/spells.js
+++ js/spells.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const data = await loadData('../data/full-spells.json')
-    console.log(data[0])
-    function generateSpellTable(num) {
-        let i = 0
-        for (const spell of data) {
-            i++
-            if (i == num) return
-            spellContainer.innerHTML += `
+  const data = await loadData('../data/full-spells.json')
+  console.log(data.length)
+  function generateSpellTable(num) {
+    clear_page(spellContainer)
+    let start = num
+    let end = num + 10
+    for (let i = start; i < end; i++) {
+      let spell = data[i]
+      spellContainer.innerHTML += `
             <td>
               ${spell.name}
             </td>
@@ -14,27 +15,47 @@ document.addEventListener('DOMContentLoaded', async () => {
               ${spell.level}
             </td>
             <td>
-              ${` ${spell.time[0].unit} ${spell.time[0].number}`}
-            </td>
-            <td>
-              ${spell.concentration}
+              ${` ${spell.time[0].number} ${spell.time[0].unit}`}
             </td>
             <td>
               ${spell.school}
             </td>
             <td class="onlargeonly">
-              ${`${spell.range.type} ${spell.range.amount ? spell.range.amount : ''} ${
-                spell.range.distance ? spell.range.distance.type : ''
+              ${` ${spell.range.distance.amount ? spell.range.distance.amount : ''} ${
+                spell.range.distance.type ? spell.range.distance.type : ''
               }`}
             </td>
             <td class="onlargeonly">
               ${spell.source}
             </td>
               `
-        }
     }
-    generateSpellTable(10)
-    // still needs pagination
-    // sorting by name type blah blah blah.
-    // search function
+    start = start / 10 + 1
+    generatePagination(start)
+  }
+
+  // pagination
+  function generatePagination(start) {
+    clear_page(pagination)
+    let begin = start
+    if (start >= 5) {
+      begin = start - 4
+    }
+    for (let i = begin; i <= start + 5; i++) {
+      let btn = document.createElement('button')
+      btn.innerHTML = `${i}`
+      btn.onclick = function() {
+        generateSpellTable((parseInt(this.innerHTML) - 1) * 10)
+      }
+      pagination.append(btn)
+    }
+  }
+  // sorting by name, type, school, range
+  function addSortingToButtons() {}
+
+  // search function
+  function search() {}
+
+  // start the chain of functions
+  generateSpellTable(0)
 })
